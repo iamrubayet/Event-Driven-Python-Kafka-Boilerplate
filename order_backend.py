66b4d9bef3af57d5001dd92117df2269dcbddd7b
shahriar.rubayet <shahriar.rubayet@upaybd.com>
2024-10-17 16:54:36 +0600

    order backend modified

--- order_backend.py
+++ order_backend.py
@@ -11,7 +11,7 @@ producer = KafkaProducer(bootstrap_servers='localhost:29092')
 print("Going to be generating order after every 10 seconds")
 print("will generate one unique order every 10 seconds")
 
-time.sleep(10)
+time.sleep(3)
 
 
 for i in range(1,ORDER_LIMIT):
@@ -24,4 +24,4 @@ for i in range(1,ORDER_LIMIT):
 
     producer.send(ORDER_KAFKA_TOPIC, json.dumps(data).encode('utf-8'))
     print(f"Order {i} sent to Kafka")
-    time.sleep(10)
+    time.sleep(3)
